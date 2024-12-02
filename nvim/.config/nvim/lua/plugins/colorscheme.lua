21152bd9935f6ad32f2f4c93eb91a133a18fe632
Krishom <83640457+Krish-Om@users.noreply.github.com>
2024-12-02 21:57:13 +0545

    added a discord plugin

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,12 +1,15 @@
 return {
-    {
-        "sainnhe/sonokai",
-        priority = 1000,
-        config = function()
-            vim.g.sonokai_transparent_background = "1"
-            vim.g.sonokai_enable_italic = "0"
-            vim.g.sonokai_style = "andromeda"
-            vim.cmd.colorscheme("sonokai")
-        end,
-    },
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+
+    config = function()
+      require("catppuccin").setup({
+        transparent_background = true,
+      })
+      vim.cmd.colorscheme("catppuccin-mocha")
+    end,
+  },
 }

ff7a2c6375ff46e4fc788c907f87a671f50a0cd2
Krishom <83640457+Krish-Om@users.noreply.github.com>
2024-12-03 12:25:01 +0545

    configuring neovim

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,28 +1,28 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        tag = "v0.9.1",
-        opts = {
-            ensure_installed = {
-                "javascript",
-                "typescript",
-                "css",
-                "gitignore",
-                "graphql",
-                "http",
-                "json",
-                "scss",
-                "sql",
-                "vim",
-                "lua",
-                "python",
-                "rust",
-            },
-            query_linter = {
-                enable = true,
-                use_virtual_text = true,
-                lint_events = { "BufWrite", "CursorHold" },
-            },
-        },
-    },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		tag = "v0.9.1",
+		auto_install = true,
+		opts = {
+			ensure_installed = {
+				"python",
+				"javascript",
+				"typescript",
+				"css",
+				"gitignore",
+				"graphql",
+				"http",
+				"json",
+				"sql",
+				"vim",
+				"lua",
+			},
+
+			query_linter = {
+				enable = true,
+				use_virtual_text = true,
+				lint_events = { "BufWrite", "CursorHold" },
+			},
+		},
+	},
 }

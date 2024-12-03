60ca5ed8a97f20766ae87e3e9316962ee3a8487c
Krishom <83640457+Krish-Om@users.noreply.github.com>
2024-12-03 12:40:54 +0545

    configured catppuccin opts

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -7,9 +7,23 @@ return {
 		config = function()
 			require("catppuccin").setup({
 				transparent_background = true,
+				no_bold = false,
+				no_italic = false,
+				no_underline = false,
+				default_integrations = true,
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					treesitter = true,
+					notify = false,
+					mini = {
+						enabled = true,
+						indentscope_color = "",
+					},
+				},
 			})
 			vim.cmd.colorscheme("catppuccin-mocha")
 		end,
 	},
 }
-

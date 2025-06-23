0fc6fca2417f80dc81f55731d59a9a82fe14783c
Krish-Om <krishombasukala@gmail.com>
2025-06-23 18:54:39 +0545

    amend: not using auto-wallpaper change

--- hypr/.config/hypr/UserScripts/WallpaperAutoChange.sh
+++ hypr/.config/hypr/UserScripts/WallpaperAutoChange.sh
@@ -7,33 +7,33 @@
 #
 # NOTE: this script uses bash (not POSIX shell) for the RANDOM variable
 
-wallust_refresh=$HOME/.config/hypr/scripts/RefreshNoWaybar.sh
+# wallust_refresh=$HOME/.config/hypr/scripts/RefreshNoWaybar.sh
 
-focused_monitor=$(hyprctl monitors | awk '/^Monitor/{name=$2} /focused: yes/{print name}')
+# focused_monitor=$(hyprctl monitors | awk '/^Monitor/{name=$2} /focused: yes/{print name}')
 
-if [[ $# -lt 1 ]] || [[ ! -d $1 ]]; then
-  echo "Usage:
-	$0 <dir containing images>"
-  exit 1
-fi
-
-# Edit below to control the images transition
-export SWWW_TRANSITION_FPS=60
-export SWWW_TRANSITION_TYPE=simple
-
-# This controls (in seconds) when to switch to the next image
-INTERVAL=3600
-
-while true; do
-  find "$1" |
-    while read -r img; do
-      echo "$((RANDOM % 1000)):$img"
-    done |
-    sort -n | cut -d':' -f2- |
-    while read -r img; do
-      swww img -o $focused_monitor "$img"
-      $wallust_refresh
-      sleep $INTERVAL
-
-    done
-done
+# if [[ $# -lt 1 ]] || [[ ! -d $1 ]]; then
+#   echo "Usage:
+# 	$0 <dir containing images>"
+#   exit 1
+# fi
+#
+# # Edit below to control the images transition
+# export SWWW_TRANSITION_FPS=60
+# export SWWW_TRANSITION_TYPE=simple
+#
+# # This controls (in seconds) when to switch to the next image
+# INTERVAL=3600
+#
+# while true; do
+#   find "$1" |
+#     while read -r img; do
+#       echo "$((RANDOM % 1000)):$img"
+#     done |
+#     sort -n | cut -d':' -f2- |
+#     while read -r img; do
+#       swww img -o $focused_monitor "$img"
+#       $wallust_refresh
+#       sleep $INTERVAL
+#
+#     done
+# done
